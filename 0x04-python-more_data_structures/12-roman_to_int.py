e5896db8ea780c38d6b286d6cc97f2ee8acd5877
jolah1 <jokunlade@gmail.com>
2022-07-06 12:57:42 -0700

    correction made

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -35,6 +35,6 @@ def roman_to_int(roman_string):
 
                 last_rom = rom_n.get(ch)
 
-        num += to_subtract(list_num)
+    num += to_subtract(list_num)
 
-        return (num)
+    return (num)
