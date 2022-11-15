a9d636392854a95fbde35a6b755bc57ca30fa960
jolah1 <jokunlade@gmail.com>
2022-11-15 04:02:09 -0800

    corrections made

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -12,15 +12,15 @@ request(url, function (err, response, body) {
     for (const i in tasks) {
       const task = tasks[i];
       if (task.completed === true) {
-	if (completed[task.userId] === undefined) {
+        if (completed[task.userId] === undefined) {
           completed[task.userId] = 1;
-	} else {
-	  completed[task.userId]++;
-	}
-       }
+        } else {
+          completed[task.userId]++;
+        }
       }
-      console.log(completed);
-    } else {
-      console.log('An error occured. Status code: ' + response.statusCode);
     }
+    console.log(completed);
+  } else {
+    console.log('An error occured. Status code: ' + response.statusCode);
+  }
 });
